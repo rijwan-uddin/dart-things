11051f35225a733d367a02aa3471d13a8216f37f
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-11-02 18:47:14 +0600

    D1

---
+++ prac day1.dart
@@ -0,0 +1,12 @@
+//An integer named myAge with your age.
+int myAge = 24;
+//A double named heightInMeters with your height in meters.
+double heightInMeters = 5.6;
+//A string named favoriteColor with your favorite color.
+String favouriteColor = 'navy blue';
+//A boolean named isStudent to indicate whether you're currently a student.
+bool isStudent = true;
+//Create a const variable for the value of Pi (3.14159) and name it pi.
+const double pi = 3.1416;
+//Create a final variable named currentYear and assign it to the current year using DateTime.now().year.
+final currentYear = DateTime.now();
---
+++ variables.dart
@@ -0,0 +1,8 @@
+int age = 25; // An integer variable
+double height = 5.9; // A floating-point variable
+String name = 'Rijwan'; // A string variable
+bool isStudent = true; // A boolean variable
+var message = 'hello';
+var number = '12'; //Dart infers this as int
+const double pi = 3.1213;
+final currentTime = DateTime.now();
